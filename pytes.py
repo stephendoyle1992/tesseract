2de84401dc7972bbd67269856e82cb7f49c557fd
Stephen Doyle <32746416+stephendoyle1992@users.noreply.github.com>
2018-02-23 02:03:55 -0700

    add translate option
    translate converted text via the googletrans package

--- pytes.py
+++ pytes.py
@@ -1,4 +1,5 @@
 from PIL import Image
+from googletrans import Translator
 import pytesseract
 import argparse
 import cv2
@@ -13,6 +14,12 @@ argp = argparse.ArgumentParser()
 argp.add_argument("-i", "--image", required=True, help="path to image")
 argp.add_argument("-p", "--preprocess", type=str, default="thresh",
     help="preprocessing type")
+argp.add_argument("-t", "--translate", type=str, default ="",
+    help="translate option using google trans")
+#ensure the proper tesseract trained data file is downloaded
+#(does not currently error check this for you)
+argp.add_argument("-l", "--language", type=str, default="eng",
+    help="tesseract image to string language")
 args = vars(argp.parse_args())
 
 img = cv2.imread(args["image"])
@@ -28,7 +35,10 @@ filename = "{}.png".format(os.getpid())
 cv2.imwrite(filename, gray)
 
 newimg = Image.open(filename)
-imgtext = pytesseract.image_to_string(newimg)
+imgtext = pytesseract.image_to_string(newimg, lang=args["language"])
 os.remove(filename)
 print(imgtext)
 
+if args["translate"] != "":
+    translator = Translator()
+    print(translator.translate(imgtext, src=args["translate"]))
